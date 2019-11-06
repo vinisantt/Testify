3797ceb38e95dbd37e09cab3b1794f80e72ffb43
Vinicius Dias <vdias6849@gmail.com>
2019-11-05 22:24:42 -0300

    sera

--- casosteste/settings.py
+++ casosteste/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 """
 
 import os
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -129,3 +130,6 @@ STATIC_ROOT = 'staticfiles'
 LOGOUT_REDIRECT_URL = 'index'
 LOGIN_REDIRECT_URL = 'index'
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
+
+
+django_heroku.settings(locals())
