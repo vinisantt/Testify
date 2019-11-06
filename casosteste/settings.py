e72f48b863a9b6fba7eeed1bd3b46c8a9e5ff5c9
Vinicius Dias <vdias6849@gmail.com>
2019-11-05 21:09:29 -0300

    teste

--- casosteste/settings.py
+++ casosteste/settings.py
@@ -11,7 +11,6 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 """
 
 import os
-import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -24,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'fk-2yf!8f+t0of@x)@ynhxxu02#(7lvut@(^+86iv*_s%b46=p'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
